d69609f59afae1a49afcd81506b0b12606088e0b
Palchhin <palchhinparihar@gmail.com>
2025-04-13 10:02:47 +0530

    created handlePreviousClick and handleNextClick buttons and implement them

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,27 +5,39 @@ const News = () => {
   const [news, setNews] = useState({
     articles: [],
     loading: false,
-    page: 1
+    page: 1,
+    totalResults: 0
   });
 
   useEffect(() => {
-    const fetchUsers = async () => {
-        try {
-          const response = await fetch(' https://newsapi.org/v2/top-headlines?country=us&apiKey=243a0615c67c467f970712609f4a068b');
-          const data = await response.json();
-          
-          setNews({
-            articles: data.articles,
-            loading: false,
-            page: 1
-          });
-        } catch (error) {
-          console.error('Failed to fetch users:', error);
-        }
-    };
-
-    fetchUsers();
+    fetchNews(news.page);
   }, []);
+
+  const handlePreviousClick = () => {
+    fetchNews(news.page - 1);
+  }
+
+  const handleNextClick = () => {
+    if (news.page + 1 <= Math.ceil(news.totalResults / 10)) {
+      fetchNews(news.page + 1);
+    }
+  }
+
+  const fetchNews = async (page) => {
+    try {
+      const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=243a0615c67c467f970712609f4a068b&page=${page}&pageSize=10`);
+      const data = await response.json();
+      
+      setNews({
+        articles: data.articles,
+        loading: false,
+        page: page,
+        totalResults: data.totalResults
+      });
+    } catch (error) {
+      console.error('Failed to fetch users:', error);
+    }
+  };
 
   return (
     <div className="container my-4">
@@ -38,6 +50,11 @@ const News = () => {
             <NewsItem title={article.title} desc={article.description} imageUrl={article.urlToImage} newsUrl={article.url} />
           </div>
         )})}
+      </div>
+
+      <div className="container d-flex justify-content-between px-5 my-3">
+        <button disabled={news.page <= 1} type="button" className="btn btn-dark" onClick={handlePreviousClick}>&larr; Previous</button>
+        <button type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
       </div>
     </div>
   )
